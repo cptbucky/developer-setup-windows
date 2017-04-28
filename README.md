6ceb49508002c73aef180813c4f4ca723a9bf1d7
Tom Haywood <t.haywood@avantics.co.uk>
2017-04-28 17:10:26 +0100

    atom config, registry for hidden files and extensions, posh git

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # developer-setup-windows
+
+ensure that you are able to run powershell scripts on the machine before starting, launch a powershell with admin privileges:
+
+Set-ExecutionPolicy AllSigned
+
+run chocolatey.ps1
+
+run git.ps1
--- src/chocolatey.ps1
+++ src/chocolatey.ps1
@@ -1,8 +1,8 @@
-Set-ExecutionPolicy AllSigned
-
+# install chocolatey
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-cinst git.install
 
+# install lots of apps via chocolatey
+cinst git.install
 cinst -y googlechrome
 cinst -y firefox
 cinst -y nodejs.install
@@ -14,6 +14,7 @@ cinst -y curl
 cinst -y dotnetcore
 cinst -y gitkraken
 cinst -y postman
+#faced issues with cmder possibly due to the vm sleeping..
 cinst -y cmder
 cinst -y docker-for-windows
 cinst -y dotnetcore-sdk
@@ -21,3 +22,23 @@ cinst -y ccleaner
 cinst -y sysinternals
 cinst -y microsoft-build-tools
 cinst -y nuget.commandline
+
+Install-Module posh-git
+
+# create dev directory
+mkdir c:\dev
+
+# get latest atom config
+Remove-Item -Recurse -Force ~\.atom
+git clone https://github.com/cptbucky/config-atom.git .atom
+
+# setup task bar with shortcuts
+
+# view hidden files
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v Hidden /t REG_DWORD /d 1 /f
+
+# view file extensions
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced /v HideFileExt /t REG_DWORD /d 0 /f
+
+# enable hyper-v (requires restart)
+Enable-WindowsOptionalFeature -Online -FeatureName:Microsoft-Hyper-V -All
