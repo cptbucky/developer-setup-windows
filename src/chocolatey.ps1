5a8cab7772d2888d94eb861ea60b004daf301527
cptbucky <cpt.bucky@gmail.com>
2017-04-28 11:24:54 +0100

    moved to a simple script as the process lauch didnt work and currently isnt required

--- src/chocolatey.ps1
+++ src/chocolatey.ps1
@@ -3,12 +3,21 @@ Set-ExecutionPolicy AllSigned
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 cinst git.install
 
-#Start new powershell process to insatll all choclatey packages
-$packageInstallProcess = Start-Process `
-    -FilePath "C:\Windows\system32\WindowsPowerShell\v1.0\powershell.exe" `
-    -ArgumentList @("/q", "/norestart", "packages.ps1") `
-    -PassThru `
-    -RedirectStandardOutput "packageInstallProcess.log" `
-    -RedirectStandardError $true
-
-$packageInstallProcess.WaitForExit()
+cinst -y googlechrome
+cinst -y firefox
+cinst -y nodejs.install
+cinst -y atom
+cinst -y fiddler4
+cinst -y 7zip
+cinst -y wget
+cinst -y curl
+cinst -y dotnetcore
+cinst -y gitkraken
+cinst -y postman
+cinst -y cmder
+cinst -y docker-for-windows
+cinst -y dotnetcore-sdk
+cinst -y ccleaner
+cinst -y sysinternals
+cinst -y microsoft-build-tools
+cinst -y nuget.commandline
